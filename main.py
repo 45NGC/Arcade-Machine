a2b5a3be5ed5f1e6d739e55b45a1392985a4927f
45NGC <nicolasnerfo@gmail.com>
2022-04-20 00:35:20 +0200

    Added title+fonts
    - Fix : Find a way of importing the font from the foulder 'fonts' instead of having it in the arcade-machine foulder

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import pygame, sys
 pygame.init()
 
 # Create screen
-size = (800, 800)
+size = (1000, 1000)
 screen = pygame.display.set_mode(size)
+
+# Create colors
+black = (0,0,0)
+white = (255,255,255)
+
+# Create fonts
+title_font = pygame.font.Font('ARCADECLASSIC.TTF', 100)
+
+# Create Strings
+title_string = 'ARCADE      MACHINE'
+
+# Functions
+
 
 # Main loop
 # This loop will display a screen with the name 'Arcade Machine' and a scroll of the games we can play, the games will include :
@@ -21,12 +34,17 @@ screen = pygame.display.set_mode(size)
 while True:
 	for event in pygame.event.get():
 		print(event)
-
-
-
-
-
-
-
+	
 		if event.type == pygame.QUIT:
 			sys.exit()
+	
+	# SCREEN ELEMENTS :
+
+	#	- Background:
+	screen.fill(black)
+
+	#	- Title:
+	title = title_font.render(title_string, True, white)
+	screen.blit(title, (100,50))
+
+	pygame.display.flip()
