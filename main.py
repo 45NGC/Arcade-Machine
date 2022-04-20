be69e3ec90d43fe4d73f12fd6d98db7d75bb023b
45NGC <nicolasnerfo@gmail.com>
2022-04-21 00:43:14 +0200

    Added basic buttons

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ title_string = 'ARCADE      MACHINE'
 # Functions
 
 
+
 # Main loop
 # This loop will display a screen with the name 'Arcade Machine' and a scroll of the games we can play, the games will include :
 # 	- Tetris
@@ -36,8 +37,9 @@ title_string = 'ARCADE      MACHINE'
 # 	- Add music
 
 while True:
+
 	for event in pygame.event.get():
-		#print(event)
+		print(event)
 	
 		if event.type == pygame.QUIT:
 			sys.exit()
@@ -50,14 +52,47 @@ while True:
 	#	- Background:
 	screen.fill(black)
 
+	# 	- Mouse
 	mouse = pygame.mouse.get_pos()
 
 	#	- Buttons:
-	#		- Tetris
-	if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-		pygame.draw.rect(screen,white,[width/2,height/2,140,40])
+
+	# Tetris
+	if (100 <= mouse[0] <= 350) and (350 <= mouse[1] <= 430) :
+		pygame.draw.rect(screen,white,[100,350,250,80])
 	else:
-		pygame.draw.rect(screen,grey,[width/2,height/2,140,40])
+		pygame.draw.rect(screen,grey,[100,350,250,80])
+
+	# Snake
+	if (100 <= mouse[0] <= 350) and (550 <= mouse[1] <= 630) :
+		pygame.draw.rect(screen,white,[100,550,250,80])
+	else:
+		pygame.draw.rect(screen,grey,[100,550,250,80])
+
+	# Pong
+	if (100 <= mouse[0] <= 350) and (750 <= mouse[1] <= 830) :
+		pygame.draw.rect(screen,white,[100,750,250,80])
+	else:
+		pygame.draw.rect(screen,grey,[100,750,250,80])
+
+	# Connect 4
+	if (650 <= mouse[0] <= 900) and (350 <= mouse[1] <= 430) :
+		pygame.draw.rect(screen,white,[650,350,250,80])
+	else:
+		pygame.draw.rect(screen,grey,[650,350,250,80])
+
+	# Fast Reflexes
+	if (650 <= mouse[0] <= 900) and (550 <= mouse[1] <= 630) :
+		pygame.draw.rect(screen,white,[650,550,250,80])
+	else:
+		pygame.draw.rect(screen,grey,[650,550,250,80])
+
+	# Infection
+	if (650 <= mouse[0] <= 900) and (750 <= mouse[1] <= 830) :
+		pygame.draw.rect(screen,white,[650,750,250,80])
+	else:
+		pygame.draw.rect(screen,grey,[650,750,250,80])
+
 
 	#	- Title:
 	title = title_font.render(title_string, True, white)
