d03f429c9e21bf57f36bfbb30452493463d636f4
45NGC <nicolasnerfo@gmail.com>
2025-01-10 12:34:19 +0100

    hotfix

--- game_scripts/chess.py
+++ game_scripts/chess.py
@@ -217,17 +217,17 @@ def available_squares(piece, piece_square, board_piece_positions, attacked_squar
 			else:
 
 				if x == 0 and board_piece_positions[y-piece][x+1] < 0 and piece == 1:
-					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x], piece):
+					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x+1], piece):
 						available_squares['coordinates'].append((square_coordinates[y-piece],square_coordinates[x+1]))
 				if x == 7 and board_piece_positions[y-piece][x-1] < 0 and piece == 1:
-					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x], piece):
+					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x-1], piece):
 						available_squares['coordinates'].append((square_coordinates[y-piece],square_coordinates[x-1]))
 
 				if x == 0 and board_piece_positions[y-piece][x+1] > 0 and piece == -1:
-					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x], piece):
+					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x+1], piece):
 						available_squares['coordinates'].append((square_coordinates[y-piece],square_coordinates[x+1]))
 				if x == 7 and board_piece_positions[y-piece][x-1] > 0 and piece == -1:
-					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x], piece):
+					if  not is_king_on_check_after_move(board_piece_positions, turn, [y, x], [y-piece, x-1], piece):
 						available_squares['coordinates'].append((square_coordinates[y-piece],square_coordinates[x-1]))
 			
 			# ON PEASANT CAPTURE
